4c75842927af24be25db03b539a03d7f3b930b0d
Krisztian Kiss <kisskrisztian215@gmail.com>
2023-11-14 20:54:15 +0100

    Finished Update Profile Page

--- src/pages/UpdateProfile/UpdateProfile.jsx
+++ src/pages/UpdateProfile/UpdateProfile.jsx
@@ -1,8 +1,239 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import UserHeader from "../../components/UserHeader";
+import {
+  Container,
+  FormWrapper,
+  InputContainer,
+  ImageInputContainer,
+  Label,
+  ImageInput,
+  ImageLabel,
+  ImageInputTextContainer,
+  ImageInputIcon,
+  ImageInputTitle,
+  ImageInputDescription,
+  ButtonContainer,
+  Button,
+  InputWrapper,
+  Input,
+  ErrorMessage,
+} from "./UpdateProfile.styles";
+import axios from "axios";
 
 const UpdateProfile = () => {
-  return <UserHeader title="Update Profile">UpdateProfile</UserHeader>;
+  const initialFormData = {
+    username: "",
+    address: "",
+    phoneNumber: "",
+    email: "",
+    profilePicture: "",
+  };
+
+  const [formData, setFormData] = useState(initialFormData);
+  const [errors, setErrors] = useState({
+    phoneNumber: "",
+    email: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleImageInputChange = (e) => {
+    const file = e.target.files[0];
+    setFormData((prevData) => ({
+      ...prevData,
+      profilePicture: file,
+    }));
+  };
+
+  const validateEmail = (email) => {
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return emailRegex.test(email);
+  };
+
+  const validatePhoneNumber = (phoneNumber) => {
+    const phoneRegex = /^\d{10}$/;
+    return phoneRegex.test(phoneNumber);
+  };
+
+  useEffect(() => {
+    const emailIsEmpty = formData.email.length === 0;
+    const phoneIsEmpty = formData.phoneNumber.length === 0;
+
+    if (!emailIsEmpty) {
+      const isEmailValid = validateEmail(formData.email);
+      if (!isEmailValid) {
+        setErrors((prevState) => ({
+          ...prevState,
+          email: "Please provide a valid email format",
+        }));
+      } else {
+        setErrors((prevState) => ({
+          ...prevState,
+          email: "",
+        }));
+      }
+    } else {
+      setErrors((prevState) => ({
+        ...prevState,
+        email: "",
+      }));
+    }
+
+    if (!phoneIsEmpty) {
+      const isPhoneValid = validatePhoneNumber(formData.phoneNumber);
+      if (!isPhoneValid) {
+        setErrors((prevState) => ({
+          ...prevState,
+          phoneNumber: "Please provide a valid phone number format",
+        }));
+      } else {
+        setErrors((prevState) => ({
+          ...prevState,
+          phoneNumber: "",
+        }));
+      }
+    } else {
+      setErrors((prevState) => ({
+        ...prevState,
+        phoneNumber: "",
+      }));
+    }
+  }, [formData]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (errors.email.length === 0 && errors.phoneNumber.length === 0) {
+      console.log("Form submitted with data:", formData);
+      handleData(formData);
+      setFormData(initialFormData);
+    }
+  };
+
+  const handleData = async (data) => {
+    try {
+      const response = await axios.put(
+        "http://localhost:3500/api/user/update",
+        data,
+        {
+          headers: {
+            "x-auth-token": ``,
+          },
+        }
+      );
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <UserHeader title="Update Profile">
+      <Container>
+        <FormWrapper onSubmit={handleSubmit}>
+          <InputContainer>
+            <Label>Photo</Label>
+            <ImageInputContainer>
+              <ImageLabel htmlFor="imageinput">
+                <ImageInputTextContainer>
+                  <ImageInputIcon>
+                    <svg
+                      stroke="currentColor"
+                      fill="none"
+                      stroke-width="2"
+                      viewBox="0 0 24 24"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      class="text-3xl text-emerald-500"
+                      height="1.4em"
+                      width="1.4em"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      {" "}
+                      <polyline points="16 16 12 12 8 16"></polyline>{" "}
+                      <line x1="12" y1="12" x2="12" y2="21"></line>{" "}
+                      <path d="M20.39 18.39A5 5 0 0 0 18 9h-1.26A8 8 0 1 0 3 16.3"></path>{" "}
+                      <polyline points="16 16 12 12 8 16"></polyline>{" "}
+                    </svg>
+                  </ImageInputIcon>
+                  <ImageInputTitle>Drag your image here</ImageInputTitle>
+                  <ImageInputDescription>
+                    (Only *.jpeg and *.png images will be accepted)
+                  </ImageInputDescription>
+                </ImageInputTextContainer>
+              </ImageLabel>
+              <ImageInput
+                name="profilePicture"
+                id="imageinput"
+                hidden
+                type="file"
+                onChange={handleImageInputChange}
+              />
+            </ImageInputContainer>
+          </InputContainer>
+          <InputWrapper>
+            <InputContainer>
+              <Label name="username">Full Name</Label>
+              <Input
+                name="username"
+                placeholder="Full Name"
+                value={formData.username}
+                onChange={handleInputChange}
+                value={formData.username}
+              />
+            </InputContainer>
+            <InputContainer>
+              <Label name="address">Address</Label>
+              <Input
+                name="address"
+                placeholder="Address"
+                value={formData.address}
+                onChange={handleInputChange}
+                value={formData.address}
+              />
+            </InputContainer>
+          </InputWrapper>
+          <InputWrapper>
+            <InputContainer>
+              <Label name="phoneNumber">Phone/Mobile</Label>
+              <Input
+                name="phoneNumber"
+                placeholder="Phone/Mobile"
+                value={formData.phoneNumber}
+                onChange={handleInputChange}
+                value={formData.phoneNumber}
+              />
+              {errors.phoneNumber && (
+                <ErrorMessage>{errors.phoneNumber}</ErrorMessage>
+              )}
+            </InputContainer>
+            <InputContainer>
+              <Label name="email">Email Address</Label>
+              <Input
+                name="email"
+                placeholder="Email Address"
+                value={formData.email}
+                onChange={handleInputChange}
+                value={formData.email}
+              />
+              {errors.email && <ErrorMessage>{errors.email}</ErrorMessage>}
+            </InputContainer>
+          </InputWrapper>
+          <ButtonContainer>
+            <Button onClick={handleSubmit} type="submit">
+              Update Profile
+            </Button>
+          </ButtonContainer>
+        </FormWrapper>
+      </Container>
+    </UserHeader>
+  );
 };
 
 export default UpdateProfile;
--- src/pages/UpdateProfile/UpdateProfile.styles.js
+++ src/pages/UpdateProfile/UpdateProfile.styles.js
@@ -1,1 +1,118 @@
 import styled from "styled-components";
+import breakpoints from "../../styles/breakpoints";
+
+export const Container = styled.div``;
+
+export const FormWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.9rem;
+`;
+
+export const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.3rem;
+  width: 100%;
+`;
+
+export const ImageInputContainer = styled.div`
+  width: 100%;
+  height: 130px;
+  border: 2px dashed #d1d5db;
+  border-radius: 0.3rem;
+`;
+
+export const ImageInputTextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const ImageInput = styled.input``;
+
+export const ImageInputIcon = styled.div`
+  color: ${(props) => props.theme.emeraldGreen};
+`;
+
+export const ImageInputTitle = styled.p`
+  font-size: 0.7rem;
+`;
+
+export const ImageInputDescription = styled.p`
+  margin-top: 0.2rem;
+  font-size: 0.6rem;
+  color: ${(props) => props.theme.lightGray};
+`;
+
+export const ImageLabel = styled.label`
+  display: block;
+  width: 100%;
+  height: 100%;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Input = styled.input`
+  box-sizing: border-box;
+  width: 100%;
+  font-size: 0.7rem;
+  border-radius: 0.3rem;
+  padding: 0.6rem 1rem;
+  border: 1px solid #e5e7eb;
+
+  &:focus {
+    outline: 1px solid ${(props) => props.theme.emeraldGreen};
+  }
+
+  &::placeholder {
+    font-size: 0.65rem;
+  }
+`;
+
+export const Label = styled.label`
+  font-size: 0.7rem;
+  color: ${(props) => props.theme.mediumGray};
+  font-weight: 500;
+`;
+
+export const Button = styled.button`
+  background-color: ${(props) => props.theme.emeraldGreen};
+  border-radius: 0.2rem;
+  padding: 0.75rem 1rem;
+  border: none;
+  cursor: pointer;
+  color: #ffff;
+  font-size: 0.7rem;
+  transition: color 0.2s ease-in-out;
+  width: 100%;
+  &:hover {
+    background-color: ${(props) => props.theme.darkGreen};
+  }
+  @media only screen and ${breakpoints.device.xxs} {
+    width: auto;
+  }
+`;
+
+export const ButtonContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+`;
+
+export const ErrorMessage = styled.p`
+  color: ${(props) => props.theme.red};
+  font-size: 0.65rem;
+`;
+
+export const InputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  width: 100%;
+  @media only screen and ${breakpoints.device.xxs} {
+    flex-direction: row;
+  }
+`;
